d8a1440e48e295b01f6fda25b60740cf42e53601
victor.castro <victorcappelletto@gmail.com>
2024-06-17 14:42:28 -0300

    Alteração exercicio 01

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,7 +1,7 @@
 
 # Crie programa que o usuário digita o seu nome e retorna o número de caracteres
 
-nome_usuario=input("Digite seu nome:")
+nome_usuario=input("Digite seu nome: ")
 tamanho_nome=(int(len(nome_usuario)))
 
 print(f'Olá {nome_usuario} seu nome possui o tamanho de {tamanho_nome}')
