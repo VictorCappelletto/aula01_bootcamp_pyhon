36d694a55bcb13961aa53d7c02a2392093c69c7e
victor.castro <victorcappelletto@gmail.com>
2024-06-17 14:24:00 -0300

    adição do exercicio 03

---
+++ Exercicio03.py
@@ -0,0 +1,10 @@
+
+# Exercício 03: Refatore o exercício 02 atribuindo variáveis
+
+print('iniciando o programa')
+numero_1=float(input('Digite o primeiro numero: '))
+numero_2=float(input('Digite o primeiro numero: '))
+soma_numero=numero_1+numero_2
+
+print(f'A soma do primeiro numero {numero_1} com o segundo numero {numero_2} é de {soma_numero:.2f}!')
+exit
