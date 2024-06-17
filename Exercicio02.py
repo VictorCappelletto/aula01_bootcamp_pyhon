97902f05a429945f6804f35ee3b29d11186c465f
victor.castro <victorcappelletto@gmail.com>
2024-06-17 14:23:32 -0300

    adição do exercicio 02

---
+++ Exercicio02.py
@@ -0,0 +1,10 @@
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print('iniciando o programa')
+numero_1=float(input('Digite o primeiro numero: '))
+numero_2=float(input('Digite o primeiro numero: '))
+soma_numero=numero_1+numero_2
+
+print(f'A soma do primeiro numero {numero_1} com o segundo numero {numero_2} é de {soma_numero:.2f}!')
+exit
